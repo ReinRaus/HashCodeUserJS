04e2e1eee8c4387d14bd41035c9870f44c41ac1c
ReinRaus <reinraus.nax@gmail.com>
2013-11-07 04:34:21 +0400

    В фильтр адресов добавлен research (исследования)

--- addons/newAnswersAndComments.js
+++ addons/newAnswersAndComments.js
@@ -1,6 +1,6 @@
 // @author Yura Ivanov
 function __newAnswersAndComments() {
-  var regexURL= new RegExp("^https?://[^/]+/questions/.*$", "i");
+  var regexURL= new RegExp("^https?://[^/]+/(?:questions|research)/.*$", "i");
   if (!regexURL.test(location.href)) return; // если не вопрос, то не работаем
   var qid = $("a.post-vote.up").attr("href").replace('/vote/', '').replace('/up', '');
   var qopened = window.localStorage.getItem('__opened_' + qid);
