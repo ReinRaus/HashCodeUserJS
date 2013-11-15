ba5907ea41fb3b91a3bfbe4518a6382a83db9557
www-data <www-data@hivede12-3188.fornex.org>
2013-11-15 21:56:23 +0400

    Automatic build 36

--- release/Opera.user.js
+++ release/Opera.user.js
@@ -213,7 +213,7 @@ function __addonLoader() {
     if (typeof $ == 'undefined') return;
     clearInterval(__checkStartedInterval);
     __addonsStarted= true;
-    var build= parseInt("35");
+    var build= parseInt("36");
     window.addEventListener("message", function(message, url){
         if (message.data.substring(0, 12)=="SetSettings:"){
             var settings= JSON.parse(message.data.substring(12));
