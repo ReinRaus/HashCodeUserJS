9ebb8ef5b85f85feb0b6d5d16fbb24ce01685363
ReinRaus <reinraus.nax@gmail.com>
2013-11-16 00:42:50 +0400

    Теперь пробуем в мастер

--- userjsloader.js
+++ userjsloader.js
@@ -261,7 +261,7 @@ function __addonLoader() {
             window[__addons[i]]();
         }
     }
-};
+};//
 
 document.addEventListener( "DOMContentLoaded", __addonLoader, false );
 var __check__Started= function(){
