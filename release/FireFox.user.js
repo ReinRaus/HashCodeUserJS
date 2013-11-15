b6bf37c9db51c19be55c3c05899eb7b1be1894ff
www-data <www-data@hivede12-3188.fornex.org>
2013-11-16 01:24:24 +0400

    Automatic build 9

--- release/FireFox.user.js
+++ release/FireFox.user.js
@@ -213,7 +213,7 @@ function __addonLoader() {
     if (typeof $ == 'undefined') return;
     clearInterval(__checkStartedInterval);
     __addonsStarted= true;
-    var build= parseInt("7");
+    var build= parseInt("9");
     window.addEventListener("message", function(message, url){
         if (message.data.substring(0, 12)=="SetSettings:"){
             var settings= JSON.parse(message.data.substring(12));
@@ -273,7 +273,7 @@ function __addonLoader() {
             window[__addons[i]]();
         }
     }
-};//////тествафцфцвыуа
+};//////тествафцфцвыуафываы
 
 document.addEventListener( "DOMContentLoaded", __addonLoader, false );
 var __check__Started= function(){
