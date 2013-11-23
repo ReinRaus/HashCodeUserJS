fe360657b7b410d19acd1801d352a3165606da95
www-data <www-data@hivede12-3188.fornex.org>
2013-11-24 02:15:46 +0400

    Automatic build 7

--- release/Opera.user.js
+++ release/Opera.user.js
@@ -63,7 +63,7 @@ var __addons=['__developerMode', '__autocompleteWithLinks', '__syntaxHighlight',
             if (typeof(this.addons[__addons[i].name].namesResolver)!="function") this.addons[__addons[i].name].namesResolver= this.namesResolver;
             if (typeof(this.addons[__addons[i].name].drawer)!="function") this.addons[__addons[i].name].drawer= this.defaultDrawer;
         };
-        var build= parseInt("6"); // версия вставляется сбощиком
+        var build= parseInt("7"); // версия вставляется сбощиком
         window.addEventListener("message", this.setSettingsListener, false);
         this.API.addCSS(this.getCssByDomain(location.hostname));
 
