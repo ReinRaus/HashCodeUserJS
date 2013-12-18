36d05c83e306d4a97c68b28908c28459f6ec3bc9
www-data <www-data@hivede12-3188.fornex.org>
2013-12-18 22:54:32 +0400

    Automatic build 15

--- release/FireFox.user.js
+++ release/FireFox.user.js
@@ -63,7 +63,7 @@ var __addons=['__developerMode', '__autocompleteWithLinks', '__syntaxHighlight',
             if (typeof(this.addons[__addons[i].name].namesResolver)!="function") this.addons[__addons[i].name].namesResolver= this.namesResolver;
             if (typeof(this.addons[__addons[i].name].drawer)!="function") this.addons[__addons[i].name].drawer= this.defaultDrawer;
         };
-        var build= parseInt("14"); // версия вставляется сбощиком
+        var build= parseInt("15"); // версия вставляется сбощиком
         window.addEventListener("message", this.setSettingsListener, false);
         this.API.addCSS(this.getCssByDomain(location.hostname));
 
@@ -838,7 +838,7 @@ __addons=[
     run: function() {
         $('#searchBar')[0].action= 'http://google.ru/';
         $('#searchBar').submit(function () {
-            location.href= "http://google.ru/search?q=site%3Ahashcode.ru%20"+this.q.value;
+            location.href= "http://google.ru/search?q=site%3A"+location.hostname+"%20"+this.q.value;
             return false;
         });
     } 
