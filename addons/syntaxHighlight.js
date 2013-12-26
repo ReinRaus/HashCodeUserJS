bdfba34bf32d5380513c942b4ffcb01919eb4ccc
ReinRaus <reinraus.nax@gmail.com>
2013-12-27 02:53:48 +0400

    Fix: name, title

--- addons/syntaxHighlight.js
+++ addons/syntaxHighlight.js
@@ -1,6 +1,6 @@
 ﻿{
-    name: 'syntaxHighlight2',
-    title: 'Подсветка синтаксиса SyntaxHighlighter\'ом2',
+    name: 'syntaxHighlight',
+    title: 'Подсветка синтаксиса SyntaxHighlighter\'ом',
     description: 'Автопределение языка подсветки по тэгам вопроса\nПоддержка языков: text/plain, html+js, js, c/c++/objective-c, c#, ruby, python, php, pascal/delphi/freepascal',
     settings: {
         'usePretty' : '1',
